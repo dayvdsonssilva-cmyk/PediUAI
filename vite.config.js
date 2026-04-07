ed2097ac356aa97340a6bb1ba66e991691e23c01
dayvdsonssilva-cmyk <dayvdson.s.silva@gmail.com>
2026-04-07 13:57:47 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,15 +3,17 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   root: '.',
   publicDir: 'public',
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: {
+        main:    'index.html',
+        cliente: 'cliente.html',
+      }
+    }
+  },
   server: {
     port: 3000,
     open: true
-  },
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets'
-  },
-  esbuild: {
-    charset: 'utf8'
   }
 });
