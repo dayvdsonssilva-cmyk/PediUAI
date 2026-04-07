dac30b325864de63d6bd1c91da3973dd898e9444
dayvdsonssilva-cmyk <dayvdson.s.silva@gmail.com>
2026-04-07 17:52:56 -0300

    Update supabase.js

--- src/supabase.js
+++ src/supabase.js
@@ -1,12 +1,11 @@
 // src/supabase.js
 import { createClient } from '@supabase/supabase-js';
 
-const SUPA_URL = 'https://nmttkjmfazcipefeakkx.supabase.co';
-const SUPA_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5tdHRram1mYXpjaXBlZmVha2t4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQ3MTM3NjQsImV4cCI6MjA5MDI4OTc2NH0.MMTX_6iQJk7Uv3HPSk0m32_BihvqsWhHJ_qiRkw0WYo';
+const URL = import.meta.env.VITE_SUPABASE_URL;
+const KEY = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
-let _client = null;
-
+let _c = null;
 export function getSupa() {
-  if (!_client) _client = createClient(SUPA_URL, SUPA_KEY);
-  return _client;
+  if (!_c) _c = createClient(URL, KEY);
+  return _c;
 }
