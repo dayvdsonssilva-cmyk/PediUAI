bf23d96cf0721f61841c8e45a5bd8c29c9f34c5c
dayvdsonssilva-cmyk <dayvdson.s.silva@gmail.com>
2026-04-07 11:41:48 -0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,14 +1,18 @@
 // src/main.js
-import { goTo, openDemo, showToast } from './utils.js';
+import { goTo, openDemo, showToast, showTab, copiarLink, salvarConfig, atualizarSlugPreview, atualizarLinkDash } from './utils.js';
 import { doLogin, doRegister } from './auth.js';
 import { initDashboard } from './dashboard.js';
 
-// Expõe globalmente para os onclick do HTML
-window.goTo       = goTo;
-window.openDemo   = openDemo;
-window.doLogin    = doLogin;
-window.doRegister = doRegister;
-window.showToast  = showToast;
+window.goTo                 = goTo;
+window.openDemo             = openDemo;
+window.showToast            = showToast;
+window.showTab              = showTab;
+window.copiarLink           = copiarLink;
+window.salvarConfig         = salvarConfig;
+window.atualizarSlugPreview = atualizarSlugPreview;
+window.atualizarLinkDash    = atualizarLinkDash;
+window.doLogin              = doLogin;
+window.doRegister           = doRegister;
 
 document.addEventListener('DOMContentLoaded', () => {
   console.log('🚀 PEDIWAY iniciado!');
